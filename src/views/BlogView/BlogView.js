63bd1e25ba8557373df65e97fdc51c203b6bab53
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-06-09 17:03:09 +0530

    added title and author name and img in  BlogView.js

--- src/views/BlogView/BlogView.js
+++ src/views/BlogView/BlogView.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import blogs from './../../data'
+import './BlogView.css'
 
 function BlogView() {
-    const {id} = useParams()
+  const { id } = useParams()
 
-    const selectBlog = blogs.find((blogObject)=> blogObject.id === id)
+  const selectBlog = blogs.find((blogObject) => blogObject.id === id)
 
   return (
-    <div>
-        <h1>{selectBlog.title}</h1>
-     </div>
+    <div className='blog-detail-container'>
+      <h1 className='title'>{selectBlog.title}</h1>
+      <div className='author-container'>
+        <img src={selectBlog.author.avatar} alt='img' className='author-image' />
+        <div>
+          <div>
+            <span className='name'> {selectBlog.author.name} </span>
+            <span className='dot'>.</span>
+            <span className='follow'>Follow</span>
+          </div>
+          <div>
+            <span className='dim-color'> 3 min read </span>
+            <span className='dot'>.</span>
+            <span  className='dim-color'> {selectBlog.date} </span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
