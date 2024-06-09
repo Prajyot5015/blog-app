557dd9cd8e28631a32edaf8272a5ac1add843a14
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-06-09 18:09:52 +0530

    added categories

--- src/views/BlogView/BlogView.js
+++ src/views/BlogView/BlogView.js
@@ -14,36 +14,50 @@ function BlogView() {
 
   const selectBlog = blogs.find((blogObject) => blogObject.id === id)
 
+  const {
+    title,
+    content,
+    author,
+    date,
+    categories
+  } = selectBlog
+
   return (
     <div className='blog-detail-container'>
-      <h1 className='title'>{selectBlog.title}</h1>
+      <h1 className='title'>{title}</h1>
       <div className='author-container'>
-        <img src={selectBlog.author.avatar} alt='img' className='author-image' />
+        <img src={author.avatar} alt='img' className='author-image' />
         <div>
           <div>
-            <span className='name'> {selectBlog.author.name} </span>
+            <span className='name'> {author.name} </span>
             <span className='dot'>.</span>
             <span className='follow'>Follow</span>
           </div>
           <div>
             <span className='dim-color'> 3 min read </span>
             <span className='dot'>.</span>
-            <span  className='dim-color'> {selectBlog.date} </span>
+            <span className='dim-color'> {date} </span>
           </div>
         </div>
       </div>
       <div className='like-share-container'>
-        <div> 
-          <img src={ClapImg} className='like'/>
-          <img src={ChatImg} className='like'/>
+        <div>
+          <img src={ClapImg} className='like' />
+          <img src={ChatImg} className='like' />
         </div>
-        <div> 
-          <img src={SaveImg} className='icon'/>
-          <img src={PlayImg} className='icon'/>
-          <img src={ShareImg} className='icon'/>
-          <img src={MoreImg} className='icon'/>
+        <div>
+          <img src={SaveImg} className='icon' />
+          <img src={PlayImg} className='icon' />
+          <img src={ShareImg} className='icon' />
+          <img src={MoreImg} className='icon' />
         </div>
       </div>
+      <p className='blog-content'> {content} </p>
+      {
+          categories.map((category, i)=> {
+            return <span key={i} className='categories'> {category} </span>
+          })
+        }
     </div>
   )
 }
